53599b9a30f83b25478a3b3beeba5b64f9eb9751
Carlos Castro <carlos.castro@fishtownanalytics.com>
2025-09-09 08:24:55 +0000

    add part_suppliers model;

--- models/marts/int/part_suppliers.sql
+++ models/marts/int/part_suppliers.sql
@@ -1,1 +1,17 @@
-select * from {{ ref('stg_tpch_part_suppliers') }}
+with
+
+    suppliers as (select * from {{ ref("stg_tpch_suppliers") }}),
+
+    parts as (select * from {{ ref("stg_tpch_parts") }}),
+
+    part_suppliers as (select * from {{ ref("stg_tpch_part_suppliers") }}),
+
+    final as (
+        select part_suppliers.*, parts.*, suppliers.*
+        from part_suppliers
+        join suppliers on s_suppkey = ps_suppkey
+        join parts on p_partkey = ps_partkey
+    )
+
+select *
+from final
